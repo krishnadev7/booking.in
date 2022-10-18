d3357b2db698b986de370bd032c1fb52a82a67b0
krishnadev v <krishnadevv12@gmail.comgit config --global user.name krishnadev7>
2022-10-18 10:32:51 +0530

    3rd commit: Homepage list completed

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import Feature from '../../components/featured/Feature';
+import FeaturedProperty from '../../components/featuredProperty/FeaturedProperty';
 import Header from '../../components/header/Header';
 import Navbar from '../../components/navbar/Navbar';
+import PropertyList from '../../components/propertyList/PropertyList';
+import './home.css'
 
 function Home() {
   return (
     <div>
       <Navbar />
       <Header />
+      <div className='homeContainer'>
+        <Feature />
+        <h1 className='homeTitle'>Browse by property type</h1>
+        <PropertyList />
+        <h1 className='homeTitle'>Homes guests love</h1>
+        <FeaturedProperty/>
+      </div>
     </div>
   );
 }
