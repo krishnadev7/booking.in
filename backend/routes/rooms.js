c76ee95962bbed08d3635d1011a2e43004ca31bb
krishnadev v <krishnadevv12@gmail.comgit config --global user.name krishnadev7>
2022-10-23 11:55:29 +0530

    13th commit: created room controller & model

--- backend/routes/rooms.js
+++ backend/routes/rooms.js
@@ -1,5 +1,25 @@
 import express from 'express';
 
+import { createRoom, deleteRoom, getAllRoom, getRoom, updateRoom } from '../controllers/room.js';
+
+import { verifyAdmin } from '../utlis/verifyToken.js';
+
 const router = express.Router();
 
+// Creating
+router.post('/:hotelid', verifyAdmin, createRoom);
+
+// Updating
+router.put('/:id', verifyAdmin, updateRoom);
+
+// Delete
+router.delete('/:id/:hotelid', verifyAdmin, deleteRoom);
+
+// Get
+router.get('/:id', getRoom);
+
+// Get All
+router.get('/', getAllRoom);
+
 export default router;
+
